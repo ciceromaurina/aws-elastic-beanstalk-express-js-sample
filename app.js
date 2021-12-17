adfd743031a219ff1fbd819bb78987981c80e3e8
Cicero Maurina <ciceromaurina@hotmail.com>
2021-12-16 23:52:36 -0300

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Aplicação CiceroMaurina!'));
+app.get('/', (req, res) => res.send('Aplicação CiceroPipeline!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
